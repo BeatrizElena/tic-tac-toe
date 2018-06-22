3403429cbb2a581b2b01af2b279e16721c93f501
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-22 13:33:24 -0400

    Write CURL script for changing password
    It looks as if server is not set up to allow for changing
    password. Issue need to be investigated further if time allows.

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,20 @@
+# It looks like server does not allow me to change password
+
+# ID=2 sh curl-scripts/change-password.sh
+
+# command to use on terminal:
+# $ TOKEN="" NEW_PASSWORD="12345" sh curl-scripts/json/sign-up.sh
+
+curl "http://tic-tac-toe.wdibos.com/change-password" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"\
+  --data '{
+    "passwords": {
+      "old": "'"${OLD_PASSWORD}"'",
+      "new": "'"${NEW_PASSWORD}"'"
+      }
+    }'
+
+echo
