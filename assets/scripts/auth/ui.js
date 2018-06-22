e182d8923048d72d0378a22ad1ec06bcdd80088e
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-22 15:30:03 -0400

    Add onSignInSuccess function to ui.js
    Tells user that she signed in successfully.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -8,6 +8,13 @@ const signUpSuccess = function (data) {
   console.log(`signUpSuccess ran. Data is: ${data}`)
 }
 
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully')
+  $('#message').css('background-color', 'green')
+  console.log(`signInSuccess ran. Data is: ${data}`)
+}
+
 module.exports = {
-  signUpSuccess
+  signUpSuccess,
+  signInSuccess
 }
