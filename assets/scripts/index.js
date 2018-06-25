fcd5e95e1dfc61dd686f649cc6775ac6e22866a2
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-25 16:55:34 -0400

    Add event handler to index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,9 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events')
+const gameBoard = require('./game')
 
 $(() => {
   authEvents.addHandlers()
+  gameBoard()
 })
