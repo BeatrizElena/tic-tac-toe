b8391059a5ccc76164c865b1d6b0ef34045606fc
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-25 09:59:55 -0400

    Include correct terminal command

--- curl-scripts/change-password.sh
+++ curl-scripts/change-password.sh
@@ -1,9 +1,5 @@
-# It looks like server does not allow me to change password
-
-# ID=2 sh curl-scripts/change-password.sh
-
 # command to use on terminal:
-# $ TOKEN="" NEW_PASSWORD="12345" sh curl-scripts/json/sign-up.sh
+# $ TOKEN="BAhJIiU0ZTJlMTg0NzYxNzI0ZDY3NGVmODQ2YWFiYjg2YmI1OAY6BkVG--a7a49a3f2bf26f9632b9249296789d093ed42dbd" OLD_PASSWORD="12345" NEW_PASSWORD="123" sh curl-scripts/change-password.sh
 
 curl "https://tic-tac-toe-wdi.herokuapp.com//change-password" \
   --include \
