7bd9cf8ce312b7d959108e2f5c9d6a164f6ed1bd
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-26 18:04:40 -0400

    Work on winner function - no functionality yet
     Includes Danny's suggestions
     Needs to fix code so there are no two click events on the same
     elements as is the case now.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,32 +1,34 @@
 'use strict'
 let moves = 0
-const minMoves = 5
-const ticTacToeWin = [
+const ticTacToeWin =
   {
-    ttt1: [0, 1, 2]
-  },
-  {
-    ttt2: [3, 4, 5]
-  },
-  {
-    ttt3: [6, 7, 8]
-  },
-  {
-    ttt4: [0, 3, 6]
-  },
-  {
-    ttt5: [1, 4, 7]
-  },
-  {ttt6: [2, 5, 8]
-  },
-  {
-    ttt7: [0, 4, 8]
-  },
-  {
+    ttt1: [0, 1, 2],
+    ttt2: [3, 4, 5],
+    ttt3: [6, 7, 8],
+    ttt4: [0, 3, 6],
+    ttt5: [1, 4, 7],
+    ttt6: [2, 5, 8],
+    ttt7: [0, 4, 8],
     ttt8: [2, 4, 6]
   }
-]
-const game = []
+const gamex = []
+const gameo = []
+// let square
+
+const winner = function (event) {
+  // const square =
+  $('.box').on('click', function (event) {
+    // console.log(event.target.id)
+    const square = event.target.id
+    const message = $('#winner') // .html('')
+    if ($(`#${square}`).length >= 5) {
+      if (gamex === ticTacToeWin.ttt0 || `#${square}` === ticTacToeWin.ttt1 || `#${square}` === ticTacToeWin.ttt2 || `#${square}` === ticTacToeWin.ttt3 || `#${square}` === ticTacToeWin.tt4 || `#${square}` === ticTacToeWin.ttt5 || `#${square}` === ticTacToeWin.ttt6 || `#${square}` === ticTacToeWin.ttt7 || `#${square}` === ticTacToeWin.ttt8) {
+        // inside if
+        $(`#${message}`).html('You win!').css('color', 'yellow')
+      }
+    }
+  })
+}
 
 const gameBoard = function () {
   // target square
@@ -37,12 +39,16 @@ const gameBoard = function () {
       // when moves = 0, modulo is 0 or player one.
       // moves increases by 1, so modulo is 1 or player 2
       moves % 2 === 0 ? $(`#${square}`).html('X').css('color', 'red') : $(`#${square}`).html('O').css('color', 'yellow')
-      game.push()
+      if ($(`#${square}`).text() === 'X') {
+        gamex.push(`#${square}`)
+      } else {
+        gameo.push(`#${square}`)
+      }
       moves++
     } else if ($(`#${square}`).text() !== '') {
       $(`#${square}`).off()
     }
-  })
+  }).winner()
 }
 
 module.exports = gameBoard
