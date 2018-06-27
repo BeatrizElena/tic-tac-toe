1ff7aab845713e18e6404290164b4078dee2ec19
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-27 10:29:56 -0400

    Add onSuccess and onFailure functions for every interaction with the API
    Follow syntax and logic from examples given

---
+++ assets/scripts/games/game-ui.js
@@ -0,0 +1,74 @@
+'use strict'
+
+const onCreateSuccess = function (data) {
+  $('#message').text('Game successfully created')
+  $('#message').css('background-color', 'green')
+  console.log('onCreateSuccess ran. Data is :', data)
+}
+
+const onCreateFailure = function (error) {
+  $('#message').text('Error on creating game')
+  $('#message').css('background-color', 'red')
+  console.error('onCreateFailure ran. Error is :', error)
+}
+
+const onIndexSuccess = function (data) {
+  $('#message').text('All games successfully received')
+  $('#message').css('background-color', 'green')
+  console.log('onIndexSuccess ran. Data is :', data.examples)
+}
+
+const onIndexFailure = function (error) {
+  $('#message').text('Error on getting games')
+  $('#message').css('background-color', 'red')
+  console.error('onIndexFailure ran. Error is :', error)
+}
+
+const onShowSuccess = function (data) {
+  $('#message').text('One game successfully received')
+  $('#message').css('background-color', 'green')
+  console.log('onCreateSuccess ran. Data is :', data)
+}
+
+const onShowFailure = function (error) {
+  $('#message').text('Error on getting game')
+  $('#message').css('background-color', 'red')
+  console.error('onShowFailure ran. Error is :', error)
+}
+
+const onDestroySuccess = function () {
+  $('#message').text('Game successfully deleted')
+  $('#message').css('background-color', 'green')
+  console.log('Game successfully deleted')
+}
+
+const onDestroyFailure = function (error) {
+  $('#message').text('Error on deleting game')
+  $('#message').css('background-color', 'red')
+  console.error('onDestroyFailure ran. Error is :', error)
+}
+
+const onUpdateSuccess = function () {
+  $('#message').text('Game successfully updated')
+  $('#message').css('background-color', 'green')
+  console.log('Game successfully updated')
+}
+
+const onUpdateFailure = function (error) {
+  $('#message').text('Error on updating game')
+  $('#message').css('background-color', 'red')
+  console.error('onUpdateFailure ran. Error is :', error)
+}
+
+module.exports = {
+  onCreateSuccess,
+  onCreateFailure,
+  onIndexSuccess,
+  onIndexFailure,
+  onShowSuccess,
+  onShowFailure,
+  onDestroySuccess,
+  onDestroyFailure,
+  onUpdateSuccess,
+  onUpdateFailure
+}
