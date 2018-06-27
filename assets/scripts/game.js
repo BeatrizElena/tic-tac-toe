c899007f945f14c7035f309d8a571ce832a7b3da
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-26 22:48:45 -0400

    Add win function
    Unable to test because switchPlayer function seems broken and app is not
    compiling.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,54 +1,59 @@
 'use strict'
-let moves = 0
-const ticTacToeWin =
-  {
-    ttt1: [0, 1, 2],
-    ttt2: [3, 4, 5],
-    ttt3: [6, 7, 8],
-    ttt4: [0, 3, 6],
-    ttt5: [1, 4, 7],
-    ttt6: [2, 5, 8],
-    ttt7: [0, 4, 8],
-    ttt8: [2, 4, 6]
-  }
-const gamex = []
-const gameo = []
-// let square
+// let moves = 0
+// const ticTacToeWin =
+//   {
+//     ttt1: [0, 1, 2],
+//     ttt2: [3, 4, 5],
+//     ttt3: [6, 7, 8],
+//     ttt4: [0, 3, 6],
+//     ttt5: [1, 4, 7],
+//     ttt6: [2, 5, 8],
+//     ttt7: [0, 4, 8],
+//     ttt8: [2, 4, 6]
+//   }
 
-const winner = function (event) {
-  // const square =
-  $('.box').on('click', function (event) {
-    // console.log(event.target.id)
-    const square = event.target.id
-    const message = $('#winner') // .html('')
-    if ($(`#${square}`).length >= 5) {
-      if (gamex === ticTacToeWin.ttt0 || `#${square}` === ticTacToeWin.ttt1 || `#${square}` === ticTacToeWin.ttt2 || `#${square}` === ticTacToeWin.ttt3 || `#${square}` === ticTacToeWin.tt4 || `#${square}` === ticTacToeWin.ttt5 || `#${square}` === ticTacToeWin.ttt6 || `#${square}` === ticTacToeWin.ttt7 || `#${square}` === ticTacToeWin.ttt8) {
-        // inside if
-        $(`#${message}`).html('You win!').css('color', 'yellow')
-      }
-    }
-  })
+const win = function () {
+  const message = $('#winner') // .html('')
+  const x1 = $('#0, #1, #2').text()
+  const x2 = $('#3, #4, #5').text()
+  const x3 = $('#6, #7, #8').text()
+  const x4 = $('#0, #3, #6').text()
+  const x5 = $('#1, #4, #7').text()
+  const x6 = $('#2, #5, #8').text()
+  const x7 = $('#0, #4, #8').text()
+  const x8 = $('#2, #4, #6').text()
+  const o1 = $('#0, #1, #2').text()
+  const o2 = $('#3, #4, #5').text()
+  const o3 = $('#6, #7, #8').text()
+  const o4 = $('#0, #3, #6').text()
+  const o5 = $('#1, #4, #7').text()
+  const o6 = $('#2, #5, #8').text()
+  const o7 = $('#0, #4, #8').text()
+  const o8 = $('#2, #4, #6').text()
+  if (x1 === 'XXX' || x2 === 'XXX' || x3 === 'XXX' || x4 === 'XXX' || x5 === 'XXX' || x6 === 'XXX' || x7 === 'XXX' || x8 === 'XXX') {
+    $(`#${message}`).html('X, you win!').css('color', 'yellow')
+  } else if (o1 === 'OOO' || o2 === 'OOO' || o3 === 'OOO' || o4 === 'OOO' || o5 === 'OOO' || o6 === 'OOO' || o7 === 'OOO' || o8 === 'OOO') {
+    $(`#${message}`).html('O, you win!').css('color', 'green')
+  }
 }
 
-const gameBoard = function () {
-  // target square
+// when moves = 0, modulo is 0 or player one.
+// moves increases by 1, so modulo is 1 or player 2
+
+const switchPlayer = function () {
+  let moves = 0
   $('.box').on('click', function (event) {
-    // console.log(event.target.id)
-    const square = event.target.id
+    const square = event.target.id // target square
     if ($(`#${square}`).text() === '') {
-      // when moves = 0, modulo is 0 or player one.
-      // moves increases by 1, so modulo is 1 or player 2
       moves % 2 === 0 ? $(`#${square}`).html('X').css('color', 'red') : $(`#${square}`).html('O').css('color', 'yellow')
-      if ($(`#${square}`).text() === 'X') {
-        gamex.push(`#${square}`)
-      } else {
-        gameo.push(`#${square}`)
-      }
       moves++
     } else if ($(`#${square}`).text() !== '') {
       $(`#${square}`).off()
     }
-  }).winner()
+  }
 }
 
-module.exports = gameBoard
+module.exports = {
+  switchPlayer,
+  win
+}
