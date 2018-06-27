76047162a5de5ff8b0195ba86ff39b1963ce720d
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-26 22:46:50 -0400

    Change function name from gameBoard to switchPlayer
    Game logic is currently broken.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,9 +6,9 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events')
-const gameBoard = require('./game')
+const switchPlayer = require('./game')
 
 $(() => {
   authEvents.addHandlers()
-  gameBoard()
+  switchPlayer()
 })
