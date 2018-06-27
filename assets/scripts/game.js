379ccc5616a30296c47b83e80514218892852227
beatrizelena <ortizwebdevelopment@gmail.com>
2018-06-27 12:14:09 -0400

    Win function is now functioning correctly
    Had to move function from outside event listener to inside event
    listener.
    Added off('click') to entire game grid so when a winner is declared,
    no square is clickable.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -13,27 +13,22 @@
 //   }
 
 const win = function () {
-  const message = $('#winner') // .html('')
   const x1 = $('#0, #1, #2').text()
+  console.log(x1)
   const x2 = $('#3, #4, #5').text()
   const x3 = $('#6, #7, #8').text()
+  console.log(x3)
   const x4 = $('#0, #3, #6').text()
   const x5 = $('#1, #4, #7').text()
   const x6 = $('#2, #5, #8').text()
   const x7 = $('#0, #4, #8').text()
   const x8 = $('#2, #4, #6').text()
-  const o1 = $('#0, #1, #2').text()
-  const o2 = $('#3, #4, #5').text()
-  const o3 = $('#6, #7, #8').text()
-  const o4 = $('#0, #3, #6').text()
-  const o5 = $('#1, #4, #7').text()
-  const o6 = $('#2, #5, #8').text()
-  const o7 = $('#0, #4, #8').text()
-  const o8 = $('#2, #4, #6').text()
   if (x1 === 'XXX' || x2 === 'XXX' || x3 === 'XXX' || x4 === 'XXX' || x5 === 'XXX' || x6 === 'XXX' || x7 === 'XXX' || x8 === 'XXX') {
-    $(`#${message}`).html('X, you win!').css('color', 'yellow')
-  } else if (o1 === 'OOO' || o2 === 'OOO' || o3 === 'OOO' || o4 === 'OOO' || o5 === 'OOO' || o6 === 'OOO' || o7 === 'OOO' || o8 === 'OOO') {
-    $(`#${message}`).html('O, you win!').css('color', 'green')
+    $('#message').html('X, you win!').css('color', 'yellow')
+    $('.box').off('click')
+  } else if (x1 === 'OOO' || x2 === 'OOO' || x3 === 'OOO' || x4 === 'OOO' || x5 === 'OOO' || x6 === 'OOO' || x7 === 'OOO' || x8 === 'OOO') {
+    $('#message').html('O, you win!').css('color', 'green')
+    $('.box').off('click')
   }
 }
 
@@ -50,8 +45,8 @@ const switchPlayer = function () {
     } else if ($(`#${square}`).text() !== '') {
       $(`#${square}`).off()
     }
+    win()
   })
-  win()
 }
 
 module.exports = {
