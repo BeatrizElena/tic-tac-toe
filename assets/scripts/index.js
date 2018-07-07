113797f06244f18f3761713746a73546468a5ee5
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-07 17:17:32 -0400

    Remove unneeded code that had been commented out

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,13 +6,6 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-// game-events.js has:
-// $('#game-create').on('submit', onCreateBoard) ===> generates table
-// $('#game-create').on('submit', onCreateGame)
-// $('#game-index').on('submit', onIndexGame)
-// $('#game-show').on('submit', onShowGame)
-// $('#game-update').on('submit', onUpdateGame)
-
 const authUsers = require('./auth/events')
 const gameEvents = require('./games/game-events')
 
