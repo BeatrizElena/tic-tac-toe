f5edce321b42459789f5b183d44e461db07be306
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-07 17:14:22 -0400

    Initial work on game logic
    This file (table.js) is no longer needed as all functionality
    was moved to game-logic.js

--- assets/scripts/table.js
+++ assets/scripts/table.js
@@ -3,8 +3,6 @@
 const generateTable = function () {
   // get the reference for the html element where the table will go
   const game = document.querySelector('#game-div')
-  console.log(game)
-  console.log("Generate table!")
 
   // create table and tbody elements
   const table = document.createElement('table')
@@ -25,10 +23,11 @@ const generateTable = function () {
       // const position = 'R' + i + 'C' + j
       const position = [i, j]
       id++
-      const cellText = document.createTextNode(id)
-      cell.appendChild(cellText)
+      // commenting out next two lines bc cells should be empty
+      // const cellText = document.createTextNode(id)
+      // cell.appendChild(cellText)
       row.appendChild(cell)
-      cell.setAttribute('border', '2')
+      cell.setAttribute('id', id) // trying to set id to cell
     }
     // add row to the end of the table
     tableBody.appendChild(row)
@@ -40,20 +39,7 @@ const generateTable = function () {
   const gameDivId = document.getElementById('game-div')
   game.appendChild(table)
   gameDivId.appendChild(table)
-  // let moves = 0
-  // $('table').on('click', function () {
-  //   const square = position // target square
-  //   if ($(`#${square}`).text() === '') {
-  //     moves % 2 === 0 ? $(`#${square}`).document.createTextNode('X').css('color', 'red') : $(`#${square}`).document.createTextNode('O').css('color', 'yellow')
-  //     moves++
-  //   } else if ($(`#${square}`).text() !== '') {
-  //     $(`#${square}`).off()
-  //   }
-
-  // table.setAttribute('border', '2')
-// }
 }
-
 
 module.exports = {
   generateTable
